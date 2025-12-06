cac559f5807573ae24df9a128aa8cc83150fc0d6
shantanuingole <shantanuingole23@gmail.com>
2025-12-06 13:39:33 +0530

    modified:   backend/utils/fuzzyMatch.js

--- backend/utils/fuzzyMatch.js
+++ backend/utils/fuzzyMatch.js
@@ -18,6 +18,7 @@ const levenshteinDistance = (str1, str2) => {
       if (str1[i - 1] === str2[j - 1]) {
         dp[i][j] = dp[i - 1][j - 1];
       } else {
+        
         dp[i][j] = 1 + Math.min(
           dp[i - 1][j],
           dp[i][j - 1],
